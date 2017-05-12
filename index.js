0cbd91bac9f97fc871627ebe942fd3f8d4dc28cf
Jose Alvarez <Jose Alvarez>
2017-05-11 20:14:48 -0500

    No Access-Control-Allow-Origin Error

--- index.js
+++ index.js
@@ -1,77 +1,98 @@
-var usernamesetter = $("#username");
-var emailsetter = $("#email");
-var dobsetter = $("#dob");
-var picture;
-var userID = $("#userID");
+// Roblox Variables
+var baseURL = "floating-badlands-90885.herokuapp.com​";
 
-
-function encodeImageFileAsURL(cb) {
-  return function(){
-    var file = this.files[0];
-    var reader  = new FileReader();
-    reader.onloadend = function () {
-      cb(reader.result);
-    }
-    reader.readAsDataURL(file);
-  }
-}
-
-$('#inputFileToLoad').change(encodeImageFileAsURL(function(base64Img){
-  picture = base64Img;
-}));
-
-
-
-$("#submit").on("click", function() {
-  // Local Variables
-  var firebaseRef = firebase.database();
-  var user = usernamesetter.val();
-  var email = emailsetter.val();
-  var dob = dobsetter.val();
-  var checker = "False";
-  var dataimage = picture;
-
-  // This function creates the key and stores it in the Firebase database
-  firebaseRef.ref('/Users').child(user).set({
-    username: user,
-    email: email,
-    dob: dob,
-    admin: checker,
-    pic: dataimage
+$('#roblox_Submit').on("click", function() {
+  var username = $('#roblox_Username').val();
+  var rank = $('#roblox_Rank').val();
+  var groupID = $('#roblox_Group').val();
+  var postURL = "http://floating-badlands-90885.herokuapp.com" + "/setRank/" + groupID + "/" + username + "/" + rank;
+  console.log(postURL);
+  $.ajax({
+    url: postURL,
+    dataType: 'json',
+    method: 'POST',
+  }).done (function () {
+    console.log('success');
   });
-  console.log("User: " + user);
-  console.log("Email: " + email);
-  console.log("Admin: " + checker);
-  console.log("Data Image is: " + dataimage);
-  alert(dataimage);
+
 });
 
 
-$("#searchUser").on("click", function() {
-  var user = userID.val();
-  var firebaseUserRef = firebase.database().ref('Users/' + user);
 
-  firebaseUserRef.on('value', function(snapshot) {
-    var username = snapshot.child("username").val();
-    var email = snapshot.child("email").val();
-    var dob = snapshot.child("dob").val();
-    var admin = snapshot.child("admin").val();
-    var pic = snapshot.child("pic").val();
-    var adminChecker;
-
-    if (admin == "False") {
-      adminChecker = "Nope, sorry.";
-    } else if (admin == "True") {
-      adminChecker = "Yes, you are!";
-    }
-
-
-    $("#searchedUser").html("Username: " + username);
-    $("#searchedEmail").html("Email: " + email);
-    $("#searchedDob").html("Date of Birth: " + dob);
-    $("#admin").html("Admin: " + adminChecker);
-    $("#searchedPic").html("Data Image: " + pic);
-    $("#searchedImg").attr("src", pic);
-    $("#info").css("display", "block");
-  });
-});
+// var usernamesetter = $("#username");
+// var emailsetter = $("#email");
+// var dobsetter = $("#dob");
+// var picture;
+// var userID = $("#userID");
+//
+//
+// function encodeImageFileAsURL(cb) {
+//   return function(){
+//     var file = this.files[0];
+//     var reader  = new FileReader();
+//     reader.onloadend = function () {
+//       cb(reader.result);
+//     }
+//     reader.readAsDataURL(file);
+//   }
+// }
+//
+// $('#inputFileToLoad').change(encodeImageFileAsURL(function(base64Img){
+//   picture = base64Img;
+// }));
+//
+//
+//
+// $("#submit").on("click", function() {
+//   // Local Variables
+//   var firebaseRef = firebase.database();
+//   var user = usernamesetter.val();
+//   var email = emailsetter.val();
+//   var dob = dobsetter.val();
+//   var checker = "False";
+//   var dataimage = picture;
+//
+//   // This function creates the key and stores it in the Firebase database
+//   firebaseRef.ref('/Users').child(user).set({
+//     username: user,
+//     email: email,
+//     dob: dob,
+//     admin: checker,
+//     pic: dataimage
+//   });
+//   console.log("User: " + user);
+//   console.log("Email: " + email);
+//   console.log("Admin: " + checker);
+//   console.log("Data Image is: " + dataimage);
+//   alert(dataimage);
+// });
+//
+//
+// $("#searchUser").on("click", function() {
+//   var user = userID.val();
+//   var firebaseUserRef = firebase.database().ref('Users/' + user);
+//
+//   firebaseUserRef.on('value', function(snapshot) {
+//     var username = snapshot.child("username").val();
+//     var email = snapshot.child("email").val();
+//     var dob = snapshot.child("dob").val();
+//     var admin = snapshot.child("admin").val();
+//     var pic = snapshot.child("pic").val();
+//     var adminChecker;
+//
+//     if (admin == "False") {
+//       adminChecker = "Nope, sorry.";
+//     } else if (admin == "True") {
+//       adminChecker = "Yes, you are!";
+//     }
+//
+//
+//     $("#searchedUser").html("Username: " + username);
+//     $("#searchedEmail").html("Email: " + email);
+//     $("#searchedDob").html("Date of Birth: " + dob);
+//     $("#admin").html("Admin: " + adminChecker);
+//     $("#searchedPic").html("Data Image: " + pic);
+//     $("#searchedImg").attr("src", pic);
+//     $("#info").css("display", "block");
+//   });
+// });
